b6f902c78b0ae64caadb7a93888b2fa1bfa46df0
natasyaseptiani631-eng <natasyaseptiani631@gmail.com>
2026-05-19 11:27:08 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,3 +16,16 @@ a soft return is used for the next line.
 Two (or more) newline characters in a row will result in a hard return.
 '''
 st.markdown(multi)
+
+import streamlit as st
+
+md = st.text_area('Type in your markdown string (without outer quotes)',
+                  "Happy Streamlit-ing! :balloon:")
+
+st.code(f"""
+import streamlit as st
+
+st.markdown('''{md}''')
+""")
+
+st.markdown(md)
